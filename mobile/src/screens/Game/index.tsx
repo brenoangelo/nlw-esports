189411d690e4a7dbb6a3ec7250731e09c15899d6
Breno Angelo <breno.leite@sensedata.com.br>
2022-09-19 22:09:53 -0300

    implement mobile gamecard screen

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,4 +1,4 @@
-import { Image } from 'react-native';
+import { FlatList, Image, Text } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -12,11 +12,21 @@ import { THEME } from '../../theme';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../components/Heading';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+import { useEffect, useState } from 'react';
 
 export function Game() {
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
+
   const route = useRoute();
   const navigation = useNavigation();
   const game = route.params as GameParams;
+
+  useEffect(() => {
+    fetch(`http://172.18.0.1:3333/games/${game.id}/ads`)
+      .then((response) => response.json())
+      .then((data) => setDuos(data));
+  }, []);
 
   function handleGoBack() {
     navigation.goBack();
@@ -39,7 +49,32 @@ export function Game() {
           <View style={styles.right} />
         </View>
 
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
         <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
+
+        <FlatList
+          data={duos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <DuoCard data={item} onConnect={() => {}} />
+          )}
+          horizontal
+          contentContainerStyle={[
+            duos.length > 0 ? styles.contentList : styles.emptyListContent,
+          ]}
+          style={styles.containerList}
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados para esse jogo ainda.
+            </Text>
+          )}
+        />
       </SafeAreaView>
     </Background>
   );
